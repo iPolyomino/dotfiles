f5876cbd85193e99620030aeda6112e07b921080
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2019-03-01 19:06:57 +0900

    fix export
    rust and anyenv

--- .zsh/a3_export.zsh
+++ .zsh/a3_export.zsh
@@ -6,3 +6,13 @@ export SAVEHIST=100000
 # golang
 export GOPATH="$HOME/go"
 export PATH="$PATH:$HOME/go/bin"
+
+# rust
+export PATH="$HOME/.cargo/bin:$PATH"
+export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
+
+# anyenv
+if [ -d $HOME/.anyenv ]; then
+    export PATH="$HOME/.anyenv/bin:$PATH"
+    eval "$(anyenv init -)"
+fi
