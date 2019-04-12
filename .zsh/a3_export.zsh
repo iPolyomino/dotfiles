8476e88910430eca3934c391603273f2ff17d7fe
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2019-04-12 15:34:08 +0900

    update link script

--- .zsh/a3_export.zsh
+++ .zsh/a3_export.zsh
@@ -4,12 +4,12 @@ export HISTSIZE=1000
 export SAVEHIST=100000
 
 # golang
-export GOPATH="$HOME/go"
-export PATH="$PATH:$HOME/go/bin"
+# export GOPATH="$HOME/go"
+# export PATH="$PATH:$HOME/go/bin"
 
 # rust
-export PATH="$HOME/.cargo/bin:$PATH"
-export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
+# export PATH="$HOME/.cargo/bin:$PATH"
+# export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
 
 # anyenv
 if [ -d $HOME/.anyenv ]; then
