665f5eb19beb0309c9236ca72f872142324cc7e4
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2019-05-20 14:40:00 +0900

    fzf history

--- .zsh/c2_keybinds.zsh
+++ .zsh/c2_keybinds.zsh
@@ -11,3 +11,11 @@ function ghq-fzf() {
 
 zle -N ghq-fzf
 bindkey "^g" ghq-fzf
+
+function history-fzf() {
+    BUFFER="$( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed 's/ *[0-9]* *//')"
+    zle accept-line
+}
+
+zle -N history-fzf
+bindkey "^r" history-fzf
