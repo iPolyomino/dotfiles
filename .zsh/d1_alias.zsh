9b867979fba746ee86469c0a9f3cad6d474dbf62
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2020-02-28 22:56:13 +0900

    merge macos setup

--- .zsh/d1_alias.zsh
+++ .zsh/d1_alias.zsh
@@ -1,5 +1,4 @@
 alias l="ls"
-alias ls="ls --color=auto"
 alias la="ls -AF"
 alias ll="ls -glhF"
 alias lla="ls -glhAF"
@@ -16,19 +15,25 @@ alias ....="cd ../../../"
 alias ed="emacs --daemon"
 alias e="emacsclient -t"
 alias kill-emacs="emacsclient -e '(kill-emacs)'"
+alias rawemacs="emacs -q -l ~/.rawemacs"
 
 alias g="git"
 alias v="nvim"
 
-alias pbcopy="xsel -ib"
-alias pbpaste="xsel -ob"
-
-alias open="xdg-open"
-alias x="mupdf"
-
-alias nr="/usr/sbin/service networking restart"
 alias utf8="nkf -Lu -w --overwrite"
-alias rawemacs="emacs -q -l ~/.rawemacs"
-
 alias localhost="php -S localhost:8000"
 alias lh="localhost >> /dev/null 2>&1 &"
+
+case ${OSTYPE} in
+    darwin*)
+        ;;
+    linux*)
+        alias ls="ls --color=auto"
+        alias pbcopy="xsel -ib"
+        alias pbpaste="xsel -ob"
+        alias open="xdg-open"
+        alias x="mupdf"
+
+        alias nr="/usr/sbin/service networking restart"
+        ;;
+esac
