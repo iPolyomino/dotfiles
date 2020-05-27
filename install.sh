033b742d051672d9b1b1e4d2c1af4c1eb56d033b
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2020-05-27 14:55:18 +0900

    install fundamental software

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+sudo apt update
+sudo apt upgrade
+
+sudo apt install zsh zsh-doc curl git tmux
+sudo apt install vim neovim vim-gtk emacs
+
+sudo apt install alsa-utils net-tools wireless-tools dnsutils
+
+sudo apt install chromium mutt unar gv mirage mupdf evince
+
+sudo apt install bat jq moreutils xsel
