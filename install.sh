b5665fa71a85b9643548132e151bf620b6bfd24b
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2020-07-25 11:31:54 +0900

    update install script

--- install.sh
+++ install.sh
@@ -3,20 +3,14 @@
 sudo apt update
 sudo apt upgrade
 
-sudo apt install zsh zsh-doc curl git tmux
-sudo apt install vim emacs
-
-# link dotfiles
-~/.dotfiles/link.sh
+sudo apt install zsh zsh-doc curl git tmux\
+    vim emacs fuse\
+    net-tools wireless-tools dnsutils\
+    mutt alsa-utils brightnessctl\
+    unar gv mirage mupdf evince feh imagemagick\
+    jq moreutils devscripts rsync xsel
 
 # neovim
-sudo apt install fuse
 curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
 chmod u+x nvim.appimage
 sudo mv nvim.appimage /usr/local/bin/nvim
-
-sudo apt install alsa-utils net-tools wireless-tools dnsutils
-
-sudo apt install mutt unar gv mirage mupdf evince
-
-sudo apt install jq moreutils rsync xsel
