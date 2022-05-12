0870d1bf65538d3ae44af52e2d597d0064a1e908
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2022-05-12 11:51:29 +0900

    update

--- .emacs.el
+++ .emacs.el
@@ -56,6 +56,27 @@
   (set-face-attribute 'default nil :height 200)
 )
 
+;; macOS pbcopy/pbpaste
+(when (eq system-type 'darwin)
+  (defun pbcopy ()
+    (interactive)
+    (call-process-region (point) (mark) "pbcopy")
+    (setq deactivate-mark t))
+  
+  (defun pbpaste ()
+    (interactive)
+    (call-process-region (point) (if mark-active (mark) (point)) "pbpaste" t t))
+  
+  (defun pbcut ()
+    (interactive)
+    (pbcopy)
+    (delete-region (region-beginning) (region-end)))
+  
+  (global-set-key (kbd "C-c c") 'pbcopy)
+  (global-set-key (kbd "C-c v") 'pbpaste)
+  (global-set-key (kbd "C-c x") 'pbcut)
+)
+
 ;; package setup
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
